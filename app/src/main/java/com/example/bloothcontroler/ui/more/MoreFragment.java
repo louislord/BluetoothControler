9d9a34eb15e70d2f69535701221ee76f9f657580
MrHan2 <hanwenhao@fuse.co.id>
2020-04-06 21:32:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bloothcontroler/ui/more/MoreFragment.java
@@ -0,0 +1,33 @@
+package com.example.bloothcontroler.ui.more;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.bloothcontroler.R;
+import com.example.bloothcontroler.ui.notifications.NotificationsViewModel;
+
+/**
+ * @author Hanwenhao
+ * @date 2020/3/31
+ * @Description
+ * @update [序号][日期YYYY-MM-DD] [更改人姓名][变更描述]
+ */
+public class MoreFragment extends Fragment {
+    private MoreFragmentViewModel notificationsViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(MoreFragmentViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/bloothcontroler/ui/more/MoreFragmentViewModel.java
@@ -0,0 +1,13 @@
+package com.example.bloothcontroler.ui.more;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * @author Hanwenhao
+ * @date 2020/3/31
+ * @Description
+ * @update [序号][日期YYYY-MM-DD] [更改人姓名][变更描述]
+ */
+public class MoreFragmentViewModel extends ViewModel {
+
+}
